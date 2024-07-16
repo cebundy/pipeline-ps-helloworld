ca0910e23599a6f4ec0d97e000507b04eaf9d049
Catherine Bundy <23391945+cebundy@users.noreply.github.com>
2024-07-16 14:32:25 -0700

    Create setvars.ps1

---
+++ setvars.ps1
@@ -0,0 +1,3 @@
+Write-Host "##vso[task.setvariable variable=sauce]crushed tomatoes"
+Write-Host "##vso[task.setvariable variable=secret.Sauce;issecret=true]crushed tomatoes with
+            garlic"
