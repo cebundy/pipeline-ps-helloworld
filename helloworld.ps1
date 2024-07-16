4fc0afc58b39a3262e5807bc839aa0abee77f287
Catherine Bundy <23391945+cebundy@users.noreply.github.com>
2024-07-16 14:20:55 -0700

    Update helloworld.ps1

--- helloworld.ps1
+++ helloworld.ps1
@@ -1,1 +1,9 @@
+Param(
+   [string]$sauceArgument,
+   [string]$secretSauceArgument
+)
+Write-Host No problem reading $env:SAUCE or $sauceArgument
+Write-Host But I cannot read $env:SECRET_SAUCE
+Write-Host But I can read $secretSauceArgument "(but the log is redacted so I do not
+           spoil the secret)"
 Write-Host "Hello world!"
